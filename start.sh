1091b0c22a71825f68ed879a4af7643c3c72fbe1
sdcvgc <127003702+sdcvgc@users.noreply.github.com>
2023-03-05 04:42:42 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=db2dd6d0-43d6-43d7-aafa-d74285cc98f4
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+export ProxySite=www.netsrepublic.com
+
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
